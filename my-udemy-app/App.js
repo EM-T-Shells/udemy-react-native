625dc912bf438559389fadc517b8f093d03993e2
Emre Taskale <110481472+EM-T-Shells@users.noreply.github.com>
2024-05-15 15:21:54 -0400

    added text input handlers and add goal handlers. imported state hook to update text.

--- my-udemy-app/App.js
+++ my-udemy-app/App.js
@@ -1,12 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText]= useState('');
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    console.log(enteredGoalText);
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput  style={styles.textInput} placeholder="Your Course Goal!"/>
-        <Button title="Add Goal"/>
+        <TextInput  
+        style={styles.textInput} 
+        placeholder="Your Course Goal!"
+        onChangeText={goalInputHandler}/>
+        <Button title="Add Goal" onPress={addGoalHandler}/>
       </View>
       <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
@@ -18,14 +31,14 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
-    padding: 50,
+    paddingTop: 50,
     paddingHorizontal: 16
   },
   inputContainer: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignContent: 'center',
+    alignItems: 'center',
     marginBottom: 24,
     borderBottomWidth: 1,
     borderBottomColor: '#000'
@@ -34,7 +47,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#000',
     width: '70%',
-    marginBottom: 8,
+    marginRight: 8,
     padding: 8
   },
   goalsContainer: {
