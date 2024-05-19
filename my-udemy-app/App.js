0deb7aefce910c328b734341fe992555eb33fc7f
Emre Taskale <110481472+EM-T-Shells@users.noreply.github.com>
2024-05-19 19:42:12 -0400

    debugged goal item rendering

--- my-udemy-app/App.js
+++ my-udemy-app/App.js
@@ -31,7 +31,7 @@ export default function App() {
         <FlatList 
           data={courseGoals} 
           renderItem={(itemData) => {    
-            return <GoalItem text={itemData.item.text}/>
+            return <GoalItem text={itemData.item.text} />;
           }} 
           keyExtractor={(item, index) => {
             return item.id}
--- my-udemy-app/components/GoalItem.js
+++ my-udemy-app/components/GoalItem.js
@@ -1,9 +1,11 @@
 import {StyleSheet, Text, View} from 'react-native';
 
 function GoalItem(props) {
+  return(
     <View style={styles.goalItem}>
-        <Text style={styles.goalText}>      {props.text}</Text>
+        <Text style={styles.goalText}>{props.text}</Text>
     </View>
+  );
 };
 
 export default GoalItem;
@@ -14,9 +16,6 @@ const styles = StyleSheet.create({
         padding: 8,
         borderRadius: 6,
         backgroundColor: '#5e0acc',
-        borderColor: '#000',
-        borderWidth: 1,
-        marginVertical: 8
       },
       goalText: {
         color: 'white'
